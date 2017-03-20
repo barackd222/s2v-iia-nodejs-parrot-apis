c18b24b8e35f4415e1c876aa06dd57d35bdb14d4
barackd222 <barack.dorman@gmail.com>
2017-03-20 19:37:46 +1000

    Add files via upload
    Pushing latest version

--- router/routes/parrotAPI.js
+++ router/routes/parrotAPI.js
@@ -53,6 +53,58 @@ var routes = function (app, queue) {
     res.status(202).end();
   });
 
+
+  app.get('/parrot/takeoff/:action1/:action2', function (req, res) {
+
+		try {
+
+			var action1 = req.params.action1;
+			var action2 = req.params.action2;
+
+			console.log("API + [" + req.url + "]");
+
+			if (action1 === "stay" && action2 === "land") {
+
+				// Let's make some shapes with colors:
+				queue.push(function () { parrot("takeoff-stay-land") });
+
+			} else if (action1 === "up-down" && action2 === "land") {
+
+				// Let's make some shapes with colors:
+				queue.push(function () { parrot("takeoff-up-down-land") });
+
+			} else if (action1 === "left-right" && action2 === "land") {
+
+				// Let's make some shapes with colors:
+				queue.push(function () { parrot("takeoff-left-right-land") });
+
+			} else if (action1 === "forward-backward" && action2 === "land") {
+
+				// Let's make some shapes with colors:
+				queue.push(function () { parrot("takeoff-forward-backward-land") });
+
+			} else if (action1 === "flip" && action2 === "land") {
+
+				// Let's make some shapes with colors:
+				queue.push(function () { parrot("takeoff-flip-land") });
+
+			} else {
+
+				console.log("Unknown API commands + [" + req.url + "]");
+			}
+
+
+
+
+		} catch (ex) {
+			res.status(500).json({ error: "Something went wrong!", details: ex });
+			return;
+		}
+
+    res.status(202).end();
+  });
+
+
 };
 
 module.exports = routes;  
